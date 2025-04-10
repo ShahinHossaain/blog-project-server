cde40fc48d8d2a4883c315bfc7c1a033db5fa47c
ShahinHossaain <mdshahinhossaain@gmail.com>
2025-04-10 09:53:24 +0600

    writePage is working

--- index.js
+++ index.js
@@ -16,11 +16,15 @@ app.use("/webhooks", webHookRouter);
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 app.use(clerkMiddleware());
-// new 
-
 app.use("/comment", commentRouter);
 app.use("/post", postRouter);
 app.use("/user", userRouter);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get("/auth-state", (req, res) => {
     const authState = req.auth;
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createPost, deletePost, getPost, getPosts } from '../controllers/post.controller.js';
+import { createPost, deletePost, getPost, getPosts, uploadAuth } from '../controllers/post.controller.js';
 const router = express.Router();
 
 // ALL GET REQUEST 
+router.get('/upload-auth', uploadAuth);
 router.get('/', getPosts);
 router.get('/:slug', getPost);
 
@@ -11,18 +12,5 @@ router.post('/', createPost);
 
 // ALL DELETE REQUEST 
 router.delete('/:id', deletePost);
-
-
-// router.post("/", async (req, res) => {
-//     try {
-//         const newPost = new Post(req.body);
-//         console.log("body", req.body);
-//         const savedPost = await newPost.save();
-//         res.status(201).json(savedPost);
-//     } catch (err) {
-//         console.error("Error creating post:", err);
-//         res.status(400).json({ error: "Failed to create post" });
-//     }
-// });
 
 export default router;
